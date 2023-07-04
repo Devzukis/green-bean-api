70bae2a12d62b53fddd84a5924e94273fa1c252c
Wumbo <115862489+0xWumbo@users.noreply.github.com>
2023-07-04 15:32:15 -0700

    Create endpoints to check for status and for all unclaimed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('check/:tokenId')
+  findOne(@Param('tokenId') tokenId: string) {
+    return this.appService.checkAzuki(parseInt(tokenId));
+  }
+
+  @Get('unclaimed')
+  getUnclaimed() {
+    return this.appService.getUnclaimed();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) {}
+
+  async getUnclaimed() {
+    const unclaimedAzukis = await this.prisma.azuki.findMany({
+      where: { canClaim: true },
+      select: { tokenId: true },
+    });
+    return { tokenIds: unclaimedAzukis.map((azuki) => azuki.tokenId) };
+  }
+
+  async checkAzuki(tokenId: number) {
+    const data = await this.prisma.azuki.findUnique({
+      where: { tokenId },
+      select: { canClaim: true },
+    });
+    return { canClaim: data?.canClaim };
   }
 }
