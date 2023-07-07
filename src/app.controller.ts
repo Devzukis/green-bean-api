a44aa1befdd49db4a01845d963ffb15c9e166d98
Wumbo <115862489+0xWumbo@users.noreply.github.com>
2023-07-07 02:17:44 -0500

    Add thumbnail url

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,13 +5,25 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // TODO: Deprecate
   @Get('check/:tokenId')
   findOne(@Param('tokenId') tokenId: string) {
     return this.appService.checkAzuki(parseInt(tokenId));
   }
 
+  @Get('v1/check/:tokenId')
+  findOneAzuki(@Param('tokenId') tokenId: string) {
+    return this.appService.getAzukiDetails(parseInt(tokenId));
+  }
+
+  // TODO: Deprecate
   @Get('unclaimed')
   getUnclaimed() {
     return this.appService.getUnclaimed();
+  }
+
+  @Get('v1/can-claim')
+  getAzukisThatCanClaim() {
+    return this.appService.getAllCanClaimAzukis();
   }
 }
