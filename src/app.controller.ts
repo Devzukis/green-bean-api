5907f0a9f8e6c5cd1eb2c137f2a2326b7a5b4f43
Wumbo <115862489+0xWumbo@users.noreply.github.com>
2023-07-09 23:18:39 -0700

    Remove deprecated endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,21 +5,9 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // TODO: Deprecate
-  @Get('check/:tokenId')
-  findOne(@Param('tokenId') tokenId: string) {
-    return this.appService.checkAzuki(parseInt(tokenId));
-  }
-
   @Get('v1/check/:tokenId')
   findOneAzuki(@Param('tokenId') tokenId: string) {
     return this.appService.getAzukiDetails(parseInt(tokenId));
-  }
-
-  // TODO: Deprecate
-  @Get('unclaimed')
-  getUnclaimed() {
-    return this.appService.getUnclaimed();
   }
 
   @Get('v1/can-claim')
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,29 +5,12 @@ import { PrismaService } from './prisma.service';
 export class AppService {
   constructor(private prisma: PrismaService) {}
 
-  async getUnclaimed() {
-    const unclaimedAzukis = await this.prisma.azuki.findMany({
-      where: { canClaim: true },
-      select: { tokenId: true },
-      orderBy: { tokenId: 'asc' },
-    });
-    return { tokenIds: unclaimedAzukis.map((azuki) => azuki.tokenId) };
-  }
-
   async getAllCanClaimAzukis() {
     return await this.prisma.azuki.findMany({
       where: { canClaim: true },
       select: { tokenId: true, thumbnailUrl: true },
       orderBy: { tokenId: 'asc' },
     });
-  }
-
-  async checkAzuki(tokenId: number) {
-    const data = await this.prisma.azuki.findUnique({
-      where: { tokenId },
-      select: { canClaim: true },
-    });
-    return { canClaim: data?.canClaim };
   }
 
   async getAzukiDetails(tokenId: number) {
